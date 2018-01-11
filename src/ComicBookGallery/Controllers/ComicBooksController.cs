858961943f23edfaf5216f0a281c826ed15ac470
clare <ccreely@gmail.com>
2018-01-11 21:50:51 +0000

    Added Detail action method

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    //public is default option - needs to be otherwise it can't be found
+    public class ComicBooksController : Controller
+    {
+        //now add an 'action' method - every method on a controller is an action method
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
+    }
+}
